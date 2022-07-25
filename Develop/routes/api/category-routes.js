2d540c2d03de2ac57c6bfd7a0af4e0738b40f270
msnaye <99111936+msnaye@users.noreply.github.com>
2022-07-24 23:11:59 -0500

    created get one

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -13,14 +13,42 @@ router.get('/', (req, res) => {
         attributes: ['product_name']
       }
     ]
+  }).then(dbCatData =>{
+    if (!dbCatData){
+      res.status(404).json({ message: 'No user matching that id'});
+      return;
+    }
+    res.json(dbCatData);
+  }).catch(err=> {
+    console.log(err);
+    res.status(500).json(err);
   })
-});
+  });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-});
-
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['product_name']
+      }
+    ]
+  }).then(dbCatData =>{
+    if (!dbCatData) {
+      res.status(404).json({message:''No user matching that id''})
+      return;
+    }
+    res.json(dbCatData);
+  }).catch(err=> {
+    console.log(err);
+    res.status(500).json(err);
+  })
+  });
 router.post('/', (req, res) => {
   // create a new category
 });
