b0a0f51c043ec4309a2a5636c57c0500a3dd0bfa
msnaye <99111936+msnaye@users.noreply.github.com>
2022-07-24 23:28:40 -0500

    added categories

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -60,14 +60,42 @@ router.post('/', (req, res) => {
       res.status(500).json(err);
     });
   });
-
-
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body,{
+    where:{
+      id: req.params.id
+    }
+  })
+  .then(dbCatData=>{
+    if (!dbCatData[0]){
+      res.status(404).json({message:'No user matching that id'});
+      return;
+    }
+    res.json(dbCatData);
+  }).catch(err=> {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
-
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({ 
+    where:{
+      id: req.params.id
+    }
+  })
+  .then(dbCatData => {
+    if(!dbCatData){
+      res.status(404).json({message:'No user matching that id'});
+      return;
+    }
+    res.json(dbCatData);
+  })
+  .catch(err =>{
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
