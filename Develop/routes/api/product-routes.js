4104b9af78708ac74c9574ba838e39c758842411
msnaye <99111936+msnaye@users.noreply.github.com>
2022-07-26 15:58:06 -0500

    worked on product routes, category and started on tag routes-also tested on insomnia

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,13 +7,62 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-});
+  Product.findAll({
+    include:[
+      { 
+        model: Category,
 
+        attributes: ['category_name']
+      },
+  
+      {
+        model:Tag,
+        attributes: ['tag_name']
+      }
+    ]
+  }).then(dbProData =>{
+    if (!dbProData){
+      res.status(404).json({ message: 'No product matching that id'});
+      return;
+    }
+    res.json(dbProData);
+  }).catch(err=> {
+    console.log(err);
+    res.status(500).json(err);
+  })
+  });
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-});
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include:[
+      { 
+        model: Category,
+
+        attributes: ['category_name']
+      },
+  
+      {
+        model:Tag,
+        attributes: ['tag_name']
+      }
+    ]
+  }).then(dbCatData =>{
+    if (!dbCatData) {
+      res.status(404).json({message:'No category matching that id'})
+      return;
+    }
+    res.json(dbCatData);
+  }).catch(err=> {
+    console.log(err);
+    res.status(500).json(err);
+  })
+  });
+
 
 // create new product
 router.post('/', (req, res) => {
@@ -91,6 +140,19 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({ 
+    where: { id: req.params.id}})
+    .then(dbProData => {
+      if(!dbProData){
+        res.status(404).json({message:'No product matching that id'});
+        return;
+      }
+      res.json(dbProData);
+    })
+    .catch(err =>{
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
